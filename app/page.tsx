b72057d471b3374c385208dd2cf79d4f541507e4
ziga.vidmar <zigavidmar96@gmail.com>
2024-10-20 22:12:13 +0200

    Destructure strategies map

--- app/page.tsx
+++ app/page.tsx
@@ -62,29 +62,29 @@ export default async function Page() {
       <div className="container mx-auto">
         <h1 className="text-2xl font-bold pb-5">Bot Strategies</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {botStrategies.map((bot) => (
-            <Link href={`/bot-strategies/${bot.id}`} key={bot.id} className="bg-foreground p-4 rounded-lg">
+          {botStrategies.map(({ id: botId, created_at, name, profit, totalTrades }) => (
+            <Link href={`/bot-strategies/${botId}`} key={botId} className="bg-foreground p-4 rounded-lg">
               <div>
-                <h2 className="text-xl font-bold text-[#a5a6b5]">{bot.name}</h2>
-                <p className="text-gray-500 text-sm">Created {moment(bot.created_at).fromNow()}</p>
+                <h2 className="text-xl font-bold text-[#a5a6b5]">{name}</h2>
+                <p className="text-gray-500 text-sm">Created {moment(created_at).fromNow()}</p>
                 <p className="text-lg font-bold mt-2 flex items-center gap-2">
                   Profit: 
-                  {bot.profit > 0 && (
-                    <span className="text-green-500">+{bot.profit}%</span>
+                  {profit > 0 && (
+                    <span className="text-green-500">+{profit}%</span>
                   )}
-                  {bot.profit < 0 && (
-                    <span className="text-red-500">-{bot.profit}%</span>
+                  {profit < 0 && (
+                    <span className="text-red-500">-{profit}%</span>
                   )}
-                  {bot.profit === 0 && (
+                  {profit === 0 && (
                     <span className="text-white">/</span>
                   )}
                 </p>
                 <p className="text-gray-400 text-sm pt-2">
-                  {bot.totalTrades === 0 && (
+                  {totalTrades === 0 && (
                     <span className="text-red-500">No trades yet</span>
                   )}
-                  {bot.totalTrades > 0 && (
-                    <span>Total trades: <b className="text-white">{bot.totalTrades}</b></span>
+                  {totalTrades > 0 && (
+                    <span>Total trades: <b className="text-white">{totalTrades}</b></span>
                   )}
                 </p>
               </div>
